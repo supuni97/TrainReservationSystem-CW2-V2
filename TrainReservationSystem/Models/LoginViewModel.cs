0bb2fe9ae1d31d186becb6bc758e4982cec0d1fc
supuni97 <sitharabandara1997@gmail.com>
2026-07-26 20:40:48 +0530

    Added login viewmodel and accountController

---
+++ TrainReservationSystem/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TrainReservationSystem.Models;
+
+public class LoginViewModel
+{
+    [Required]
+    [Display(Name = "Username")]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; } = string.Empty;
+
+    public string? ErrorMessage { get; set; }
+}
